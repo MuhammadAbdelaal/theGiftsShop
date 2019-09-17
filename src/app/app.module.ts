4c63685b72b340302f7c6586f37ee08c6f798204
Muhammad Reda Abdelaal <trans.muhammad@gmail.com>
2019-09-17 13:31:14 +0200

    adding navbar and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,16 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  { path: '',  redirectTo: '/home',  pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'cart', component: ShoppingCartComponent},
+  {path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +36,7 @@ import { NotfoundComponent } from './notfound/notfound.component';
     NotfoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule
   ],
