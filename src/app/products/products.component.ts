f58fa6fe5ed67c8ee393487ff7a1cece9bea89a4
Muhammad Reda Abdelaal <trans.muhammad@gmail.com>
2019-10-08 19:28:54 +0200

    adding products to produtcs page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+declare const $: any;
+
 
 @Component({
   selector: 'app-products',
@@ -7,11 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() {
+  products: Array<any> = [];
+  type: string = 'Men';
+  menClicked:boolean = true;
+  womenClicked: boolean = false;
+
+  modalImage: String = "";
+  modalPrice: Number;
+  modalBrand: String = "";
+  modalDesc: String = "";
+  modalLongDesc: String = "";
+
+  constructor(_ProductsService: ProductsService) {
     window.scroll(0,0); //prevent scrolling dowon as default behavior
+
+    this.products = _ProductsService.products;
    }
 
   ngOnInit() {
+
+
   }
 
+
+  setType(type:string){
+    this.type = type;
+    this.menClicked = !this.menClicked;
+    this.womenClicked = !this.womenClicked;
+  }
+
+  
+  setModalData(imagUrl:String, Price:Number, Brand: String, Desc: String, LongDesc: String) {
+    this.modalImage = imagUrl;
+    this.modalPrice = Price;
+    this.modalBrand = Brand;
+    this.modalDesc = Desc;
+    this.modalLongDesc = LongDesc;
+  }
 }
